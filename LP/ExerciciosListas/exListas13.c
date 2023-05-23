59b1cf678c4e915752224952afd611b77dc7672d
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-05-23 15:35:12 -0300

    Exercícios Lista

--- LP/ExerciciosListas/exListas13.c
+++ LP/ExerciciosListas/exListas13.c
@@ -5,6 +5,26 @@
 
 int main() {
     double mediaMensal[12];
+    double somaMediasMensais = 0;
     double mediaAnual;
     int contadorMedias;
+
+    // Aqui eu faço um vetor de Strings, que nada mais são do que um conjunto/vetor de chars
+    // Dessa forma, o valor de meses[0] é Janeiro, meses[1] é Fevereiro e assim por diante
+    char* meses[12] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+    for (int i = 0; i < 12; i++) {
+        printf("Digite a temperatura média de %s: ", meses[i]);
+        scanf("%lf", &mediaMensal[i]);
+        somaMediasMensais += mediaMensal[i];
+    }
+
+    mediaAnual = somaMediasMensais / 12;
+
+    printf("\nA temperatura média anual foi de %.2lf°C.\n\n", mediaAnual);
+    for (int i = 0; i < 12; i++) {
+        if (mediaMensal[i] > mediaAnual) {
+            printf("A temperatura de %.2lf°C em %s foi acima da média.\n", mediaMensal[i], meses[i]);
+        }
+    }
 }
---
+++ LP/ExerciciosListas/exListas14.c
@@ -0,0 +1,64 @@
+// Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+// "Telefonou para a vítima?"
+// "Esteve no local do crime?"
+// "Mora perto da vítima?"
+// "Devia para a vítima?"
+// "Já trabalhou com a vítima?"
+// O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
+// Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
+
+#include <stdio.h>
+
+int main() {
+    int qntdPositiva = 0;
+    char* perguntas[5] = {"Telefonou para a vítima?", "Esteve no local do crime?", "Mora perto da vítima?", "Devia para a vítima?", "Já trabalhou com a vítima?"};
+
+    printf("Responda as perguntas com \"S\" para sim ou \"N\" para não\n");
+    for (int i = 0; i < 5; i++) {
+        char resposta;
+        printf("%s ", perguntas[i]);
+        scanf(" %c", &resposta);
+
+        if (resposta == 's' || resposta == 'S') {
+            qntdPositiva++;
+        }
+    }
+
+    if (qntdPositiva == 1) {
+        printf("Você respondeu positivamente a %d questão, portanto, ", qntdPositiva);
+    } else {
+        printf("Você respondeu positivamente a %d questões, portanto, ", qntdPositiva);
+    }
+    
+    // Com Switch-Case
+    switch (qntdPositiva) {
+    case 2:
+        printf("você está classificado apenas como suspeito");
+        break;
+    case 3:
+        printf("você está classificado como cúmplice");
+        break;
+    case 4:
+        printf("você está classificado como cúmplice");
+        break;
+    case 5:
+        printf("você está classificado como assassino");
+        break;
+    default:
+        printf("você está classificado como inocente");
+        break;
+    }
+
+    // Com if, else if e else
+    // if (qntdPositiva == 2) {
+    //     printf("você está classificado apenas como suspeito");
+    // } else if (qntdPositiva == 3 || qntdPositiva == 4) {
+    //     printf("você está classificado como cúmplice");
+    // } else if (qntdPositiva == 5) {
+    //     printf("você está classificado como assassino");
+    // } else {
+    //     printf("você está classificado como inocente");
+    // }
+    
+    
+}
