bd749cb2a7f46a406ac25f51954036035c5ce2a1
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-06-13 10:33:12 -0300

    Update exStrings9.c

--- LP/ExerciciosStrings/exStrings9.c
+++ LP/ExerciciosStrings/exStrings9.c
@@ -1,36 +1,37 @@
-// Verificação de CPF. Desenvolva um programa que solicite a digitação de um número de CPF no formato xxx.xxx.xxx-xx e indique se é um número válido ou inválido através da validação dos dígitos verificadores edos caracteres de formatação.
-
-
-// TODO Esse exercício ainda não está feito, faltam alguns ajustes, não leve em consideração
-
+// Verificação de CPF. Desenvolva um programa que solicite a digitação de um número de CPF no formato xxx.xxx.xxx-xx e indique se é um número válido ou inválido através da validação dos dígitos verificadores e dos caracteres de formatação.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+// Essa minha função vai retornar 1 caso o CPF seja válido e 0 caso ele seja inválido
 int validarCPF(char cpf[]) {
-    int i, j, digito1 = 0, digito2 = 0;
-    int multiplicador1 = 10, multiplicador2 = 11;
+    int digito1 = 0;
+    int digito2 = 0;
+    int multiplicador1 = 10;
+    int multiplicador2 = 11;
     int cpfNumerico[11];
+    int contadorCaracteres = 0;
 
-    // Verificar se o CPF possui 11 dígitos
+    // Verificar se o CPF possui 11 dígitos com pontos e traços
     if (strlen(cpf) != 14) {
         return 0;
     }
 
     // Converter os dígitos do CPF para valores numéricos
-    for (i = 0; i < 11; i++) {
+    // A função "isDigit(cpf[i])" retorna 1 para quando um caractere é um numero ou 0 para caso não seja. Dessa forma eu ignoro todos os pontos e traços inseridos
+    for (int i = 0; i < 14; i++) {
         if (isdigit(cpf[i])) {
-            cpfNumerico[i] = cpf[i] - '0';
-        } else {
-            return 0;
+            cpfNumerico[contadorCaracteres] = cpf[i] - '0';
+            contadorCaracteres++;
+            
         }
     }
 
     // Verificar se todos os dígitos são iguais (caso especial)
     int todosDigitosIguais = 1;
-    for (i = 1; i < 11; i++) {
+    for (int i = 1; i < 11; i++) {
         if (cpfNumerico[i] != cpfNumerico[0]) {
             todosDigitosIguais = 0;
             break;
@@ -42,14 +43,14 @@ int validarCPF(char cpf[]) {
     }
 
     // Calcular o primeiro dígito verificador
-    for (i = 0, j = 9; i < 9; i++, j--) {
+    for (int i = 0, j = 9; i < 9; i++, j--) {
         digito1 += cpfNumerico[i] * multiplicador1;
         multiplicador1--;
     }
     digito1 = (digito1 % 11 < 2) ? 0 : 11 - (digito1 % 11);
 
     // Calcular o segundo dígito verificador
-    for (i = 0, j = 10; i < 10; i++, j--) {
+    for (int i = 0, j = 10; i < 10; i++, j--) {
         digito2 += cpfNumerico[i] * multiplicador2;
         multiplicador2--;
     }
@@ -69,7 +70,7 @@ int main() {
     printf("Digite um CPF no formato xxx.xxx.xxx-xx: ");
     scanf("%s", cpf);
 
-    // Verificar se o CPF é válido
+    // Aqui eu chamo a minha outra função para verificar se o CPF é válido. Ela retornará 1 caso positivo e 0 caso negativo
     if (validarCPF(cpf)) {
         printf("CPF válido.\n");
     } else {
