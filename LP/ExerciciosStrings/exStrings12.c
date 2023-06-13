6b363dff05d06798394a4ee48b722ab53efe7f8f
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-06-13 14:40:02 -0300

    Update exStrings12.c

--- LP/ExerciciosStrings/exStrings12.c
+++ LP/ExerciciosStrings/exStrings12.c
@@ -12,7 +12,7 @@ void limparBufferEntrada() {
 int validarNumeroTelefone(char *numero) {
     int tamanho = strlen(numero);
 
-    // Verificar se o número contém apenas dígitos e possui 7 ou 8 dígitos
+    // Verificar se o número contém apenas dígitos entre 0 e nove
     for (int i = 0; i < tamanho; i++) {
         if (numero[i] < '0' || numero[i] > '9') {
             return 0; // Número inválido
@@ -28,7 +28,7 @@ void corrigirNumeroTelefone(char *numero) {
     // Verificar se o número tem 7 dígitos para adicionar o '3' na frente
     if (tamanho == 7) {
         // Realocar a string para acomodar o '3' na frente
-        for (int i = tamanho - 1; i >= 0; i--) {
+        for (int i = tamanho; i >= 0; i--) {
             numero[i + 1] = numero[i];
         }
         numero[0] = '3';
@@ -38,40 +38,46 @@ void corrigirNumeroTelefone(char *numero) {
 }
 
 int main() {
-    char numeroTelefone[9];
+    char numeroTelefone[10];
+    char numeroTelefoneSemTraco[9];
     int contadorCaracteres = 0;
 
     printf("Valida e corrige número de telefone\n");
     printf("Telefone: ");
-    fgets(numeroTelefone, 9, stdin);
+    fgets(numeroTelefone, sizeof(numeroTelefone), stdin);
 
     int tamanho = strlen(numeroTelefone);
     // Remover o traço se existir
     for (int i = 0; i < tamanho; i++) {
         if (isdigit(numeroTelefone[i])) {
-            numeroTelefone[contadorCaracteres] = numeroTelefone[i];
+            numeroTelefoneSemTraco[contadorCaracteres] = numeroTelefone[i];
             contadorCaracteres++;
         }
     }
+    numeroTelefoneSemTraco[contadorCaracteres] = '\0';
+
     tamanho = contadorCaracteres;
     
     // Remover o caractere '\n' no final da string (se existir)
-    numeroTelefone[strcspn(numeroTelefone, "\n")] = '\0';
+    numeroTelefone[strcspn(numeroTelefoneSemTraco, "\n")] = '\0';
 
     // Validar o número de telefone
-    if (!validarNumeroTelefone(numeroTelefone)) {
+    if (!validarNumeroTelefone(numeroTelefoneSemTraco)) {
         printf("Número de telefone inválido!\n");
         return 1;
     }
 
+    if (tamanho == 8) {
+        printf("Telefone sem correção: %.4s-%.4s\n", numeroTelefoneSemTraco, numeroTelefoneSemTraco+4);
+        return 0;
+    }
+
     // Corrigir o número de telefone (se necessário)
-    corrigirNumeroTelefone(numeroTelefone);
+    corrigirNumeroTelefone(numeroTelefoneSemTraco);
 
     printf("Telefone possui %d dígito(s).\n", tamanho);
-    if (strlen(numeroTelefone) == 8) {
-        printf("Telefone corrigido sem formatação: %s\n", numeroTelefone);
-        printf("Telefone corrigido com formatação: %.4s-%.4s\n", numeroTelefone, numeroTelefone+4);
-    } else {
-        printf("Telefone sem correção: %s\n", numeroTelefone);
+    if (strlen(numeroTelefoneSemTraco) == 8) {
+        printf("Telefone corrigido sem formatação: %s\n", numeroTelefoneSemTraco);
+        printf("Telefone corrigido com formatação: %.4s-%.4s\n", numeroTelefoneSemTraco, numeroTelefoneSemTraco+4);
     }
 }
